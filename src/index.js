35349c017acb563ade54382595344a8afa8a6930
Zahidmohd <btech25082.21@bitmesra.ac.in>
2026-01-04 19:22:22 +0530

    initialise project structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * Caching Proxy Server - Entry Point
+ * A CLI tool that forwards requests to an origin server and caches responses
+ */
+
+console.log('Caching Proxy Server - Initializing...');
+
+// Entry point will be implemented in Stage 2
+
